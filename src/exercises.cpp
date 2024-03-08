4d7a154f4c240723a327f4f26120e20ec38c55a0
dAHTvind888 <159574540+dAHTvind888@users.noreply.github.com>
2024-03-07 22:07:10 -0400

    exercise_6 try1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -87,6 +87,32 @@ void exercise_5(char character) {
 
 void exercise_6(int number) {
   // TODO: YOUR CODE HERE
+  switch(number){
+    case 0:
+      cout << "Invalid input" << endl;
+      break;
+    case 1:
+      cout << "Lunes" << endl;
+      break;
+    case 2:
+      cout << "Martes" << endl;
+      break;
+    case 3: 
+      cout << "Miercoles" << endl;
+      break;
+    case 4: 
+      cout << "Jueves" << endl;
+      break;
+    case 5: 
+      cout << "Viernes" << endl;
+      break;
+    case 6: 
+      cout << "Sabado" << endl;
+      break;
+    case 7: 
+      cout << "Domingo" << endl;
+      break;   
+  }
 }
 
 void exercise_7(double r) {
