67bdb2c7d55cf05f2d7e86099293dda27c87f7e2
dAHTvind888 <159574540+dAHTvind888@users.noreply.github.com>
2024-03-11 08:27:53 -0400

    exercise_10 try1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -184,6 +184,12 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
 
 int exercise_10(int a, int b) {
   // TODO: YOUR CODE HERE
+  int S = a + b;
+  string string_S = to_string(S);
+  int size_S = size(string_S);
+  int product = size_S * S;
+  
+  return product;
 }
 
 string exercise_11(int number) {
