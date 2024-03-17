33deeaa1c22d184856be87ef8014baef1206e9b8
dAHTvind888 <159574540+dAHTvind888@users.noreply.github.com>
2024-03-16 21:41:48 -0400

    exercise_16 try 24

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -312,6 +312,7 @@ void exercise_16(int debut, int fin) {
   if(cond3 == false){
     cout << "Que extraño, el inicio del alquiler es después del final..." << endl;
   }
+
   if(cond1 == true && cond2 == true && cond3 == true){
     if(0 <= debut && debut <= 7){
       if(0 <= fin && fin <= 7){
@@ -344,12 +345,13 @@ void exercise_16(int debut, int fin) {
         hour_2bs = exp_tar / 2;
       }
     }
+
     if(17 <= debut && debut <= 24){
       if(17 <= fin && fin <= 24){
         cheap_tar = (fin - debut) * 1;
         hour_1bs = cheap_tar;
         }
-      }
+    }
     
     total = hour_1bs + (hour_2bs * 2);
     cout << "Haz alquilado una bicicleta por" << endl;
@@ -361,5 +363,5 @@ void exercise_16(int debut, int fin) {
     }
     total = cheap_tar + exp_tar;
     cout << "El monto total a pagar es de " << total << " boliviano(s)." << endl;
-    }
+  }
 }
